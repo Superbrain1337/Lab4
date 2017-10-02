54575188c1963aca734439169d00552de2cd31f0
Superbrain1337 <31820507+Superbrain1337@users.noreply.github.com>
2017-10-02 21:02:17 +0200

    Created an Enemy class and created a loop in main to make them move when the player moves

---
+++ Game/Enemy.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Enemy:Entities, ILetter
+    {
+        public override int X { get; set; }
+        public override int Y { get; set; }
+        public int PrevX { get; set; }
+        public int PrevY { get; set; }
+        public char Letter { get; set; }
+        public int NumberOfEnemies { get; set; }
+        public int[,] EnemyList { get; set; }
+        public int WalkDirection { get; set; }
+
+        public Enemy()
+        {
+            Letter = 'E';
+            EnemyList = new int[2,20];
+        }
+
+        public void CreateEnemies(int difficulty)
+        {
+            while (NumberOfEnemies < 2 * difficulty)
+            {
+                Y = Rnd.Next(Board.GetLength(0));
+                X = Rnd.Next(Board.GetLength(1));
+                if (Board[Y, X] == Ruta.Empty)
+                {
+                    Board[Y, X] = Ruta.Enemie;
+                    EnemyList[0, NumberOfEnemies] = Y;
+                    EnemyList[1, NumberOfEnemies] = X;
+                    NumberOfEnemies++;
+                }
+            }
+        }
+
+        public void MoveEnemy(int currentEnemy)
+        {
+            PrevY = EnemyList[0, currentEnemy];
+            PrevX = EnemyList[1, currentEnemy];
+
+            WalkDirection = Rnd.Next(4);
+
+            if (WalkDirection == 0)
+            {
+                EnemyList[1, currentEnemy]--;
+            }
+            else if (WalkDirection == 1)
+            {
+                EnemyList[0, currentEnemy]++;
+            }
+            else if (WalkDirection == 2)
+            {
+                EnemyList[1, currentEnemy]++;
+            }
+            else
+            {
+                EnemyList[0, currentEnemy]--;
+            }
+            X = EnemyList[1, currentEnemy];
+            Y = EnemyList[0, currentEnemy];
+        }
+
+        public void UpdateEnemyPosititon(int currentEnemy)
+        {
+            if (Board[Y, X] != Ruta.Empty)
+            {
+                EnemyList[1, currentEnemy] = PrevX;
+                X = PrevX;
+                EnemyList[0, currentEnemy] = PrevY;
+                Y = PrevY;
+            }
+        }
+    }
+}
--- Game/Program.cs
+++ Game/Program.cs
@@ -15,28 +15,34 @@ namespace Game
         public static Door Door = new Door();
         public static Key Key = new Key();
         public static Empty Empty = new Empty();
+        public static Enemy Enemy = new Enemy();
 
         static void Main(string[] args)
         {
+            //Local variables is created
+            bool loseGame = false, roomComplete = false;
+            int highscore = 0, playerUsedActions = 0, roomCount = 1;
+
             //The first room in created 
             Empty.NewBoard();
             Wall.CreateRoom();
             Wall.DrawWalls();
             Door.CreateExit();
             Key.SpawnKey();
+            Enemy.CreateEnemies(roomCount);
             
-            //The local variables is created
+            //Board is created
             Entities.Ruta[,] boardGrid = Entities.Board;
-            bool loseGame = false, roomComplete = false;
+            
+            //Board is drawn on the Console
             Draw.DrawScreen(boardGrid);
             Draw.Plot(Player.X, Player.Y, Entities.Ruta.Player);
-
-            int highscore = 0, playerUsedActions = 0, roomCount = 0;
             
             while (!loseGame && roomCount < 10)
             {
                 if (roomComplete)
                 {
+                    roomCount++;
                     Player.X = 50;
                     Player.Y = 20;
                     Key.KeyCount = 0;
@@ -45,6 +51,7 @@ namespace Game
                     Wall.DrawWalls();
                     Door.CreateExit();
                     Key.SpawnKey();
+                    Enemy.CreateEnemies(roomCount);
                     Draw.DrawScreen(boardGrid);
                     Draw.Plot(Player.X, Player.Y, Entities.Ruta.Player);
                     roomComplete = false;
@@ -59,6 +66,18 @@ namespace Game
                     Draw.Plot(Player.PrevX, Player.PrevY, Entities.Ruta.Empty);
                     Draw.Plot(Player.X, Player.Y, Entities.Ruta.Player);
                 }
+
+                for (int i = 0; i < roomCount * 2; i++)
+                {
+                    Enemy.MoveEnemy(i);
+                    Enemy.UpdateEnemyPosititon(i);
+                    if (Enemy.PrevX != Enemy.X || Enemy.PrevY != Enemy.Y)
+                    {
+                        Draw.Plot(Enemy.PrevX, Enemy.PrevY, Entities.Ruta.Empty);
+                        Draw.Plot(Enemy.X, Enemy.Y, Entities.Ruta.Enemie);
+                    }
+                }
+
                 if (Player.Health <=0)
                 {
                     highscore = 1000 - playerUsedActions;
